ce710e026b4290a4b0fe5a871ebdea13195c76e8
Felipe <feelipeturmina@gmail.com>
2026-07-30 21:18:45 -0300

    Aula1

---
+++ exercicios.js
@@ -0,0 +1,198 @@
+// // Exercicio 1
+// console.log("Bem-vindo à aula de JavaScript!");
+
+// // Exercicio 2
+
+// const nome = "Felipe";
+
+// console.log(`Olá, meu nome é ${nome}`);
+
+// // Exercicio 3
+
+// const nome = "Felipe";
+// const idade = 33;
+
+// console.log(`${nome} tem ${idade} anos.`);
+
+// // Exercicio 4
+
+// const numero1 = 10;
+// const numero2 = 5;
+
+// console.log(`A soma é ${numero1 + numero2}.`);
+
+// // Exercicio 5
+
+
+// const a = 10;
+// const b = 2;
+
+// console.log(`Soma: ${a+b}`);
+// console.log(`Subtração: ${a-b}`);
+// console.log(`Multiplicação: ${a*b}`);
+// console.log(`Divisão: ${a/b}`);
+
+// Exercicio 6
+
+// const nota1 = 8;
+// const nota2 = 7;
+// const nota3 = 9;
+
+// console.log(`A média é ${(nota1 + nota2 + nota3)/3}`)
+
+// Exercicio 7
+
+// const idade = 33
+
+// console.log(`A pessoa viveu ${idade *12} meses`)
+
+
+// //Exercicio 8
+
+// const base = 10
+// const altura = 5
+
+// console.log(`A área do retângulo é ${base * altura}`)
+
+// Exercicio 9
+
+// const preco = 100
+// const desconto = 20
+
+// console.log(`O Valor final é de ${preco - desconto}`)
+
+// Exercicio 10
+
+// const salario = 2000
+// const porcentagem = 10
+
+// console.log(`O novo salário é de ${salario + (salario *(porcentagem/100))}`)
+
+// Exercicio 11
+
+// const idade = 17
+
+// if(idade >=18){
+//     console.log(`Maior de idade`)
+// }else{console.log(`Menor de idade`)
+// }
+
+// Exercicio 12
+
+// const media = 7
+
+// if(media >= 7){
+//     console.log(`Aprovado`)
+// }else{
+//     console.log(`Reprovado`)
+// }
+
+// Exercicio 13
+
+// const media = 6
+
+// if(media >= 7){
+//     console.log(`Aprovado`)
+// }
+// else if(media <= 6.9 && media >=5){
+//     console.log(`Recuperação`)
+// }else{
+//     console.log(`Reprovado`)
+// }
+
+// Exercicio 14
+
+// const numero = 2
+
+// if(numero > 0){
+//     console.log(`Positivo`)
+// }
+// else if( numero < 0){
+//     console.log(`Negativo`)
+// }else{
+//     console.log(`Zero`)
+// }
+
+// Exercicio 15
+
+// const numero = 3
+// let parImpar = 0
+
+// parImpar = numero%2
+
+// if(parImpar === 0){
+//     console.log(`O número é par`)
+// }else{
+//     console.log(`O número é ímpar`)
+// }
+
+// Exercicio 16
+
+// const numero1 = 10
+// const numero2 = 25
+
+// if(numero1 > numero2){
+//     console.log(`O maior número é ${numero1}`)
+// }else{
+//     console.log(`O maior número é ${numero2}`)
+// }
+
+// Exercicio 17
+
+// const numero1 = 35
+// const numero2 = 10
+// const numero3 = 20
+
+// if(numero1 > numero2 && numero1 > numero3){
+//     console.log(`O maior número é ${numero1}`)
+// }
+// else if (numero2 > numero1 && numero2 > numero3){
+//     console.log(`O maior número é ${numero2}`)
+// }else{
+//     console.log(`O maior número é ${numero3}`)
+// }
+
+// Exercicio 18
+
+// const usuarioBase = "Admin"
+// const senhaBase = 1234
+
+// const usuario = "Admin"
+// const senha = 1234
+
+// if(usuarioBase === usuario && senhaBase === senha){
+//     console.log(`Login realizado com sucesso`)
+// }else{
+//     console.log(`Usuário ou senha inválidos`)
+// }
+
+// Exercicio 19
+
+// const idade = 33
+
+// if(idade < 12){
+//         console.log(`Criança paga meia`)
+// }
+// else if(idade > 60){
+//     console.log(`Idoso paga meia`)
+// }else{
+//     console.log(`Adulto paga inteira`)
+// }
+
+// Exercicio 20
+
+// const peso = 70
+// const altura = 1.75
+// const imc = peso/ (altura*altura)
+
+// if(imc < 18.5){
+//     console.log(`Abaixo do peso`)
+// }
+// else if(imc > 18.4 && imc <25){
+//     console.log(`Peso normal`)
+// }
+// else if(imc > 24.9 && imc < 30){
+//     console.log(`Sobrepeso`)
+// }else{
+//     console.log(`Obesidade`)
+// }
