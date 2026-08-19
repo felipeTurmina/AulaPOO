1daa22c1c21913bfc2e65947907d4a8725d88dd9
Felipe turmina <feelipeturmina@gmail.com>
2026-08-19 15:53:08 -0300

    Refactor Livro and Emprestimo classes
    Refactor Livro and Emprestimo classes to improve naming and status handling.

--- Aula1/sistema-biblioteca.js
+++ Aula1/sistema-biblioteca.js
@@ -1,92 +1,105 @@
-const DISPONIVEL = `Disponível`
 const INDISPONIVEL = `Indisponível`
+const DISPONIVEL = `Disponível`
 
-class Livro {
-    constructor(titulo, autor, ano) {
+class Livro{
+    constructor(titulo,autor,ano){
         this.titulo = titulo
         this.autor = autor
         this.ano = ano
-        this.status = DISPONIVEL
+        this.disponivel = DISPONIVEL
     }
-
-    exibirLivro() {
-        console.log(`Título: ${this.titulo}`)
+    exibirLivro(){
+        console.log(`Titulo: ${this.titulo}`)
         console.log(`Autor: ${this.autor}`)
         console.log(`Ano: ${this.ano}`)
-        console.log(`Status: ${this.status}`)
+        console.log(`Disponibilidade: ${this.disponivel}`)
     }
-
-    marcarComoIndisponivel() {
-        this.status = INDISPONIVEL
+    marcarIndisponivel(){
+        if(this.disponivel === INDISPONIVEL){
+            return console.log(`Este livro já está indisponível`)
+        }else{
+             this.disponivel = INDISPONIVEL
+            return console.log(`Livro marcado como indisponível`)
+        }
     }
-
-    marcarComoDisponivel() {
-        this.status = DISPONIVEL
+    marcarDisponivel(){
+        if(this.disponivel === DISPONIVEL){
+            return console.log(`Este livro já está marcado como disponivel`)
+        }else{
+             this.disponivel = DISPONIVEL
+            return console.log(`Livro marcado como disponivel`)
+        }
     }
 }
 
-class Usuario {
-    constructor(nome, email) {
+class Usuario{
+    constructor(nome,email){
         this.nome = nome
         this.email = email
     }
-
-    exibirUsuario() {
-        console.log(`Usuário: ${this.nome}`)
+    exibirUsuario(){
+        console.log(`Usuario: ${this.nome}`)
         console.log(`E-mail: ${this.email}`)
     }
 }
 
-class Emprestimo {
-    constructor(usuario, livro, dataEmprestimo) {
+class Emprestimo{
+    constructor(usuario,livro,dataEmprestimo){
         this.usuario = usuario
         this.livro = livro
         this.dataEmprestimo = dataEmprestimo
-        this.status = DISPONIVEL
+        this.status = INDISPONIVEL
     }
-
-    exibirEmprestimo() {
-        console.log(`Usuário: ${this.usuario.nome}`)
-        console.log(`Livro: ${this.livro.titulo}`)
+    exibirEmprestimo(){
+        console.log(`Usuario: ${this.usuario.nome}`)
+        console.log(`Titulo do livro: ${this.livro.titulo}`)
         console.log(`Data do empréstimo: ${this.dataEmprestimo}`)
-        console.log(`Status: ${this.status}`)
+        console.log(`Disponibilidade: ${this.status}`)
     }
-
-    realizarEmprestimo() {
-        if (this.livro.status === INDISPONIVEL) {
+    realizarEmprestimo(){
+        if(this.livro.disponivel === INDISPONIVEL){
             console.log(`Livro indisponível para empréstimo.`)
             return
+        }else{
+            this.livro.marcarIndisponivel()
+            this.status = INDISPONIVEL
+            console.log(`Empréstimo realizado com sucesso.`)
         }
-        this.livro.marcarComoIndisponivel()
-        this.status = INDISPONIVEL
-        console.log(`Empréstimo realizado com sucesso.`)
     }
-    devolverLivro() {
-        if (this.livro.status === DISPONIVEL) {
-            console.log(`Este livro já está disponível.`)
+    devolverLivro(){
+        if(this.livro.disponivel === DISPONIVEL){
+            console.log(`Livro já marcado como devolvido`)
             return
+        }else{
+            this.livro.marcarDisponivel()
+            this.status = DISPONIVEL
+            console.log(`Livro marcado como devolvido`)
         }
-        this.livro.marcarComoDisponivel()
-        this.status = DISPONIVEL
-        console.log(`Livro devolvido com sucesso.`)
     }
+    
 }
 
 class Biblioteca{
     constructor(){
         this.usuario = []
-        this.titulo = []
+        this.livro = []
     }
     adicionarLivro(livro){
-        this.titulo.push(livro)
+        this.livro.push(livro)
     }
     adicionarUsuario(usuario){
         this.usuario.push(usuario)
     }
     buscarLivro(titulo){
-        return this.titulo.find(livro =>livro.titulo === titulo)
+        const encontrarLivro = this.livro.filter(livros => livros.titulo === titulo)
+        if(encontrarLivro){
+            encontrarLivro.exibirLivro()
+        }else{
+            console.log(`Livro não encontrado`)
+        }
     }
 }
+// TESTES
 
 const livro1 = new Livro(`Árvore derrubada`, `Machado de Assis`, 1899)
 const livro2 = new Livro(`Peter Pan`, `Mágico`, 1904)
@@ -106,23 +119,29 @@ livro2.exibirLivro()
 console.log(`----------------3---------------------`)
 livro3.exibirLivro()
 console.log(`----------------4---------------------`)
-usuario1.exibirUsuario()
+livro1.marcarIndisponivel()
 console.log(`----------------5---------------------`)
-usuario2.exibirUsuario()
+livro1.exibirLivro()
 console.log(`----------------6---------------------`)
-emprestimo1.exibirEmprestimo()
+usuario1.exibirUsuario()
 console.log(`----------------7---------------------`)
-emprestimo2.exibirEmprestimo()
+usuario2.exibirUsuario()
 console.log(`----------------8---------------------`)
-emprestimo3.exibirEmprestimo()
+emprestimo1.exibirEmprestimo()
 console.log(`----------------9---------------------`)
-emprestimo1.realizarEmprestimo()
-emprestimo1.exibirEmprestimo()
+emprestimo2.exibirEmprestimo()
 console.log(`----------------10--------------------`)
-emprestimo1.devolverLivro()
-emprestimo1.exibirEmprestimo()
+emprestimo3.exibirEmprestimo()
 console.log(`----------------11--------------------`)
-emprestimo2.realizarEmprestimo()
+emprestimo1.realizarEmprestimo()
 console.log(`----------------12--------------------`)
-emprestimo2.realizarEmprestimo()
+emprestimo1.exibirEmprestimo()
 console.log(`----------------13--------------------`)
+emprestimo1.devolverLivro()
+console.log(`----------------14--------------------`)
+emprestimo1.exibirEmprestimo()
+console.log(`----------------15--------------------`)
+emprestimo2.realizarEmprestimo()
+console.log(`----------------16--------------------`)
+emprestimo2.realizarEmprestimo()
+console.log(`----------------17--------------------`)
